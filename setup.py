2c46ca34f0e1e7850818dbf46c4574b9fd977c5e
Eduard Carreras <ecarreras@gisce.net>
2015-01-20 08:38:37 +0100

    Bump to v0.6.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='amoniak',
-    version='0.5.2',
+    version='0.6.0',
     packages=['amoniak', 'amoniak.caching'],
     url='http://gisce.net',
     license='MIT',
