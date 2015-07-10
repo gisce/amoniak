91dea8d1b467b326e38fe94f6bdc8e483190b52b
Eduard Carreras <ecarreras@gisce.net>
2015-07-10 09:57:53 +0200

    Fix typo

--- amoniak/caching/ot401results.py
+++ amoniak/caching/ot401results.py
@@ -2,8 +2,8 @@
 from results import OTCaching
 
 
-class OT101Caching(OTCaching):
+class OT401Caching(OTCaching):
     def __init__(self, empowering_service, mongo_connection):
-        super(OT101Caching, self).__init__(empowering_service, 'ot401_results',
+        super(OT401Caching, self).__init__(empowering_service, 'ot401_results',
                                   mongo_connection, 'ot401', 'empowering_error',
                                   'month', 'consumption')
