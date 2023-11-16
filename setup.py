690c9ef5c1481796073d6ce61cf0e16e56255858
Release <github-actions@github.com>
2023-11-16 11:56:04 +0100

    Bump to v1.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name='amoniak',
-    version='1.0.1',
+    version='1.0.2',
     packages=['amoniak', 'amoniak.caching'],
     url='http://gisce.net',
     license='MIT',
